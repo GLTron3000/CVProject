ed5046501530c3be3c5d048766f95f8a94e5490a
Thomas <gltron3000@gmail.com>
2020-11-25 15:33:39 +0100

    Bugfix

--- AA_Devoir/src/cv3000/jsf/ActivityController.java
+++ AA_Devoir/src/cv3000/jsf/ActivityController.java
@@ -18,7 +18,6 @@ import cv3000.services.IActivityManager;
 @SessionScoped
 public class ActivityController implements Serializable {
 	
-
 	private static final long serialVersionUID = 1L;
 	
 	@Inject
